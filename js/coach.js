980b6d3f558c0ed216373b5c15c19f4669723df3
rangtaraatelier51-sudo <rangtara.atelier51@gmail.com>
2026-09-07 18:13:59 +0530

    Change loadState to synchronous call in initQuiz

--- js/coach.js
+++ js/coach.js
@@ -1,6 +1,6 @@
 // AI Study Coach page logic. Relies on js/store.js being loaded first.
 async function initQuiz() {
-  state = await loadState();
+  state = loadState();
   populateSubjectSelect();
 }
 initQuiz();
